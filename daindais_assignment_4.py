ac1f3d85fbabb0d9963eba5f4024d0c8fd70d2ed
Simpkun <Talktodain@gmail.com>
2026-02-13 17:34:02 -0500

    Inital Commit
    Created the header and introduction

--- daindais_assignment_4.py
+++ daindais_assignment_4.py
@@ -1,2 +1,33 @@
-Dain Dais
-Hi
+"""
+COMP 163 - Introduction to Programming
+Assignment: Chapter 4 - College Life Adventure Game
+Name: Dain Dais
+GitHub Username: DainDais
+Date: 02/13/2026
+Description: The goal of the game is to pass your test in 5 days.
+            The game starts on monday and you'll have 4 turns per day
+            To prepare for the test. But there will be other factors to consider
+            that can determine how you perform in the end.
+AI Usage: [Describe any AI assistance OR write "None"]
+"""
+
+student_name = input("What's Your Name? ")
+current_gpa = 3.0
+study_hours = 0
+social_points = 0
+stress_level = 0
+knowledge = 0
+Energy = 70
+Day = 1
+# Introduction
+print("Professor: Congratulations on making it to the end of the semester!")
+input("Press Enter to continue...")
+print("But before I can let you enjoy your summer break you have to pass your final test.")
+input(" ")
+print(f"{student_name}: A final test? No one told me about this!")
+input(" ")
+print("Professor: If you came to class you'd know about it, and its 50 percent of your grade.")
+input(" ")
+print(f"{student_name}: Oh no! I have to pass this test or say good bye to summer!!")
+print("..........................................................................................................")
+
